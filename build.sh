6de3510dbaf558d90573318a3cbf863a4ccf66b6
Jason Erasmus <jerasmus@DESKTOP-KCM5SBB.>
2025-07-25 20:17:59 +0100

    Completed build script

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ echo "Docker is installed. Continuing build..."
 
 REPO_URL="https://github.com/jason-erasmus/Think_java_1-5.git"
 REPO_NAME="Think_java_1-5"
+CLONE_DIR="Think_java_1-5"
 
 echo "Cloning the repository..."
 rm -rf $CLONE_DIR
@@ -19,7 +20,7 @@ git clone $REPO_URL $CLONE_DIR
 cd $CLONE_DIR
 
 echo "Building Docker image..."
-docker build -t think-java
+docker build -t think-java .
 
 echo "Launching Think Java..."
 docker run -it think-java
